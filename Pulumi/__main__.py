d607306a8008866e7784e48929f170808952cd30
Anna <ane4ka08@gmail.com>
2021-02-26 18:14:11 +0200

    Add cloudfront in main.py

--- Pulumi/__main__.py
+++ Pulumi/__main__.py
@@ -1,16 +1,62 @@
 import pulumi
-from pulumi_aws import s3
+import pulumi_aws as aws
 
 import json
 
 # Create S3 Bucket
-bucket = s3.Bucket('my-bucket',
+bucket = aws.s3.Bucket('my-bucket-github',
 	acl="public-read",
 	website=s3.BucketWebsiteArgs(
 		index_document="index.html",
-		error_document="error.html",
+		error_document="404.html",
 		))
 
 # Outputs
 # Export the name of the bucket
 pulumi.export('bucket_name', bucket.id)
+
+# Create Cloudfront
+s3_origin_id = "myS3Origin-test"
+s3_distribution = aws.cloudfront.Distribution("s3Distribution-test",
+    origins=[aws.cloudfront.DistributionOriginArgs(
+        domain_name=bucket.bucket_regional_domain_name,
+        origin_id=s3_origin_id,
+    )],
+    enabled=True,
+    default_root_object="index.html",
+
+    default_cache_behavior=aws.cloudfront.DistributionDefaultCacheBehaviorArgs(
+        allowed_methods=[
+            "GET",
+            "HEAD",
+        ],
+        cached_methods=[
+            "GET",
+            "HEAD",
+        ],
+        target_origin_id=s3_origin_id,
+        forwarded_values=aws.cloudfront.DistributionDefaultCacheBehaviorForwardedValuesArgs(
+            query_string=False,
+            cookies=aws.cloudfront.DistributionDefaultCacheBehaviorForwardedValuesCookiesArgs(
+                forward="none",
+            ),
+        ),
+        viewer_protocol_policy="allow-all",
+        min_ttl=0,
+        default_ttl=3600,
+        max_ttl=86400,
+    ),
+    restrictions=aws.cloudfront.DistributionRestrictionsArgs(
+        geo_restriction=aws.cloudfront.DistributionRestrictionsGeoRestrictionArgs(
+            restriction_type="whitelist",
+            locations=[
+                "US",
+                "CA",
+                "GB",
+                "DE",
+            ],
+        ),
+    ),
+    viewer_certificate=aws.cloudfront.DistributionViewerCertificateArgs(
+        cloudfront_default_certificate=True,
+    ))
